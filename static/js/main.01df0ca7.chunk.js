00fa397cf16c354dbd4162023e164622a73232b7
vladn255 <nesvitvlad@yandex.ru>
2021-08-20 14:48:07 +0400

    Updates

--- static/js/main.01df0ca7.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonprent-car-admin"]=this["webpackJsonprent-car-admin"]||[]).push([[0],{9:function(e,t,n){"use strict";n.r(t);var r=n(0),c=n.n(r),a=n(3),s=n.n(a),i=n(1);var d=function(){return Object(i.jsx)("div",{className:"App"})};s.a.render(Object(i.jsx)(c.a.StrictMode,{children:Object(i.jsx)(d,{})}),document.getElementById("root"))}},[[9,1,2]]]);
-//# sourceMappingURL=main.01df0ca7.chunk.js.map
